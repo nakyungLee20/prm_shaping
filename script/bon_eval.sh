cc5cd29a085eb361d4b638ccf591f81c799cf605
nakyungLee20 <lnktop@snu.ac.kr>
2025-08-22 00:06:23 +0900

    0822 working backup

--- script/bon_eval.sh
+++ script/bon_eval.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#SBATCH -J olm_ev
+#SBATCH -J bon_gsm_shap_1.5b
 #SBATCH -p gpu04
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:4
 #SBATCH --ntasks=12
 #SBATCH -o /home/leena/prm_shaping/logs/%x_%A.out
 
 RUN="python"
-FILE="/home/leena/prm_shaping/inference/olym_eval_vllm.py"
+FILE="/home/leena/prm_shaping/inference/prm_gsm8k_eval_bon.py"
 
 for se in 0
 do
--- script/eval_tot.sh
+++ script/eval_tot.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#SBATCH -J 1b_shap
-#SBATCH -p gpu02
-#SBATCH --gres=gpu:1
+#SBATCH -J bon_math_loo
+#SBATCH -p gpu03
+#SBATCH --gres=gpu:4
 #SBATCH --ntasks=12
 #SBATCH -o /home/leena/prm_shaping/logs/%x_%A.out
 
 RUN="python"
-FILE="/home/leena/prm_shaping/prm_training/training.py"
+FILE="/home/leena/prm_shaping/inference/eval_scripts.py"
 
 for se in 0
 do
